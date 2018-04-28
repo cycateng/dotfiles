6f8ef9f0703c2b9bee80c77d16b0637b49d70c14
cycateng <cyber.gcw@gmail.com>
2018-04-28 15:37:21 +0000

    add ln .vim

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,5 +2,6 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-#ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.vim ~/.vim
